353b8008fd9f84ea6fc30a54523fe0c7763f8c86
Cam <camherringshaw@gmail.com>
2018-02-27 23:22:44 -0500

    Add templated routing example

--- hello_world.py
+++ hello_world.py
@@ -12,5 +12,9 @@ def hello_world():
     return render_template('index.html')
 
 
+@app.route('/<username>')
+def hello_user(username):
+    return render_template('index.html', name=username)
+
 if __name__ == '__main__':
     app.run()
